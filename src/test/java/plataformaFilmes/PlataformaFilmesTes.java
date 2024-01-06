e0ffa63a6f2e693a8c23e1701e220307bac9802d
leonardopadilha <leonardopadilha28@gmail.com>
2024-01-06 06:01:13 -0300

    Login com Map

--- src/test/java/plataformaFilmes/PlataformaFilmesTes.java
+++ src/test/java/plataformaFilmes/PlataformaFilmesTes.java
@@ -3,11 +3,17 @@ package plataformaFilmes;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PlataformaFilmesTes {
+
+    String token;
 
     @Test
     public void validarLogin() {
@@ -20,7 +26,22 @@ public class PlataformaFilmesTes {
 
         Response response = post(ContentType.JSON, jsonBody, "/auth");
 
-        String token = response.body().jsonPath().get("token");
+        token = response.body().jsonPath().get("token");
+        assertEquals(200, response.statusCode());
+        assertNotNull(token);
+    }
+
+    @BeforeAll
+    public void validarLoginMap() {
+        RestAssured.baseURI = "http://localhost:8080";
+
+        Map<String, String> map = new HashMap<>();
+        map.put("email", "aluno@email.com");
+        map.put("senha", "123456");
+
+        Response response = post(ContentType.JSON, map, "/auth");
+
+        token = response.body().jsonPath().get("token");
         assertEquals(200, response.statusCode());
         assertNotNull(token);
     }
