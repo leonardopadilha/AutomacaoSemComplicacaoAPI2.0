71ee699c969d9b5da704e0f8cab7d6fa023c0b67
leonardopadilha <leonardopadilha28@gmail.com>
2024-02-04 11:03:36 -0300

    GenericSteps.java

--- src/test/java/steps/GenericSteps.java
+++ src/test/java/steps/GenericSteps.java
@@ -1,2 +1,11 @@
-package steps;public class GenericSteps {
+package steps;
+
+import io.cucumber.java.pt.Entao;
+
+public class GenericSteps {
+    @Entao("valido que recebo status {int} no response")
+    public void validoQueReceboStatusNoResponse(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
