74f3760f44f8be73bfa66a906844b27a85810f37
leonardopadilha <leonardopadilha28@gmail.com>
2024-02-04 11:02:59 -0300

    RunnerTest.java

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -1,2 +1,16 @@
-package runner;public class RunnerTest {
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        tags = "",
+        glue = "steps",
+        plugin = {"json:target/reports/CucumberReports.json", "pretty"},
+        snippets = CucumberOptions.SnippetType.CAMELCASE
+)
+public class RunnerTest {
 }
