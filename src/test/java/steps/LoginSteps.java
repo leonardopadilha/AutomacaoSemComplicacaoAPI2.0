0aa975608c2e9ffa3082793c990971e5a4563286
leonardopadilha <leonardopadilha28@gmail.com>
2024-02-04 11:04:30 -0300

    LoginSteps.java

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,2 +1,35 @@
-package steps;public class LoginSteps {
+package steps;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class LoginSteps {
+
+    @Dado("que tenha um payload da API de Login")
+    public void queTenhaUmPayloadDaAPIDeLogin() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Quando("envio uma requisi��o do tipo POST de Login")
+    public void envioUmaRequisicaoDoTipoPOSTDeLogin() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Entao("armazeno o token que recebo do response de Login")
+    public void armazenoOTokenQueReceboDoResponseDeLogin() {
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Dado("que tenha um payload da API de Login com as seguintes informacoes")
+    public void queTenhaUmPayloadDaAPIDeLoginComAsSeguintesInformacoes(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new io.cucumber.java.PendingException();
+    }
 }
