0d143fcdcdd6de16937c58467549e28a08600dd2
leonardopadilha <leonardopadilha28@gmail.com>
2024-02-25 09:34:27 -0300

    GenericSteps.java

--- src/test/java/steps/GenericSteps.java
+++ src/test/java/steps/GenericSteps.java
@@ -1,8 +1,11 @@
 package steps;
 
+import io.cucumber.java.pt.E;
 import io.cucumber.java.pt.Entao;
 import org.junit.Assert;
 import utils.RestUtils;
+
+import java.util.ArrayList;
 
 public class GenericSteps {
     @Entao("valido que recebo status {int} no response")
@@ -13,5 +16,10 @@ public class GenericSteps {
     @Entao("valido que no campo {string} possui o valor {string}")
     public void validoQueNoCampoPossuiOValor(String key, String value) {
         Assert.assertEquals(value, RestUtils.getResponse().jsonPath().get(key));
+    }
+
+    @E("valido que recebo uma lista vazia no response")
+    public void validoQueReceboUmaListaVaziaNoResponse() {
+        Assert.assertEquals(new ArrayList<>(0), RestUtils.getResponse().jsonPath().get());
     }
 }
